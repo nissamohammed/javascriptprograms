b29f498281362155c4c4721059bd0c39a99a14eb
nissamohammed <nissamohammed8@gmail.com>
2024-11-19 14:05:05 +0530

    prime numbers

---
+++ primenumbers.js
@@ -0,0 +1,15 @@
+//print prime numbers from 0 to 100
+let count=0;
+let i,j;
+for(j=2;j<=100;j++){
+    for(i=1;i<=j;i++){
+        if(j%i==0){count++}
+    }
+    if(count==2)
+    
+        console.log(j);
+        count=0;
+    
+ 
+    
+}
