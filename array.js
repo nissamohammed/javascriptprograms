f6f0f45561bdb425327ac74c7fed84e02aa750fe
nissamohammed <nissamohammed8@gmail.com>
2024-12-04 15:47:00 +0530

    Array

---
+++ array.js
@@ -0,0 +1,29 @@
+//array
+weekdays = ['mon','tue','wed','thur']
+ console.log(weekdays);
+ // total number of items
+ console.log(weekdays.length);
+
+ //print wed from given array
+ console.log(weekdays[2]);
+
+ //add friday
+ weekdays.push('fri')
+ console.log(weekdays);
+
+ //add 3 to given array
+ weekdays.push(3)
+ console.log(weekdays);
+
+ //remove 3 from given array
+ weekdays.pop(3)
+ console.log(weekdays);
+
+ //to add 'sun' in first position
+ weekdays.unshift('sun')
+ console.log(weekdays);
+
+ //to remove 'sun' in first position
+ weekdays.shift('sun')
+ console.log(weekdays);
+
